4b0bd4048987b03751e51f37d0e037706a8e3db3
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2023-01-02 01:11:43 -0500

    Update buyvm_stock.py

--- buyvm_stock.py
+++ buyvm_stock.py
@@ -1,63 +1,41 @@
 import requests
 import json
 
-#connection string
-stockurl = requests.get('https://buyvm.hasstock.net/api/stock.json')
+while True:
+    
+    #connection string
+    stockurl = requests.get('https://buyvm.hasstock.net/api/stock.json')
 
-#pull data from connection string
-data = stockurl.text
+    #pull data from connection string
+    data = stockurl.text
 
-#convert to json format
-jsonparse = json.loads(data)
+    #convert to json format
+    jsonparse = json.loads(data)
 
-#1GB Selectors
-name_1411 = jsonparse['1411']['name']
-hasstock_1411 = jsonparse['1411']['hasStock']
-quantity_1411 = jsonparse['1411']['quantity']
+    #loops through the json data and outputs the value of each vps associated with the name, stock and quantity
+    for list in jsonparse:
+        print(list,jsonparse[list]['name'],jsonparse[list]['hasStock'],jsonparse[list]['quantity'])
 
-name_1413 = jsonparse['1413']['name']
-hasstock_1413 = jsonparse['1413']['hasStock']
-quantity_1413 = jsonparse['1413']['quantity']
-
-name_1423 = jsonparse['1423']['name']
-hasstock_1423 = jsonparse['1423']['hasStock']
-quantity_1423 = jsonparse['1423']['quantity']
-
-name_1501 = jsonparse['1501']['name']
-hasstock_1501 = jsonparse['1501']['hasStock']
-quantity_1501 = jsonparse['1501']['quantity']
-
-#2GB Selectors
-name_1402 = jsonparse['1402']['name']
-hasstock_1402 = jsonparse['1402']['hasStock']
-quantity_1402 = jsonparse['1402']['quantity']
-
-name_1414 = jsonparse['1414']['name']
-hasstock_1414 = jsonparse['1414']['hasStock']
-quantity_1414 = jsonparse['1414']['quantity']
-
-name_1424 = jsonparse['1424']['name']
-hasstock_1424 = jsonparse['1424']['hasStock']
-quantity_1424 = jsonparse['1424']['quantity']
-
-name_1502 = jsonparse['1502']['name']
-hasstock_1502 = jsonparse['1502']['hasStock']
-quantity_1502 = jsonparse['1502']['quantity']
-
-#prints the selectors
-#name is the location/size
-#hasstock is true or false if its in stock
-#quantity is how many are available
-print("1GB Ram")
-print(name_1411,"Stock?",hasstock_1411,"Quantity?",quantity_1411)
-print(name_1413,"Stock?",hasstock_1413,"Quantity?",quantity_1413)
-print(name_1423,"Stock?",hasstock_1423,"Quantity?",quantity_1423)
-print(name_1501,"Stock?",hasstock_1501,"Quantity?",quantity_1501)
-print("\n")
-print("2GB Ram")
-print(name_1402,"Stock?",hasstock_1402,"Quantity?",quantity_1402)
-print(name_1414,"Stock?",hasstock_1414,"Quantity?",quantity_1414)
-print(name_1424,"Stock?",hasstock_1424,"Quantity?",quantity_1424)
-print(name_1502,"Stock?",hasstock_1502,"Quantity?",quantity_1502)
-print("\n")
-input("Press enter to quit")
+    print("\n")
+    input_user = input("Whats the 4 digit code of the KVM you would like to see more data about? 1402 = an example:\n")
+    
+    #Will first try to parse user defined input into the 4 digit code from the json data
+    try:
+        name_vps = jsonparse[input_user]['name']
+        hasstock_vps = jsonparse[input_user]['hasStock']
+        quantity_vps = jsonparse[input_user]['quantity']
+        print(name_vps,"Is there stock?",hasstock_vps,"Whats the quantity?",quantity_vps)
+        
+        #typing 'restart' will restart the program or type anything else will exit the program 
+        restart_var = input("Press enter to quit or type the word 'restart' to start over\n")
+        if restart_var == 'restart': 
+            continue
+        else : 
+            break
+    
+    #If that fails then it will give some syntax and an option to restart but if they type 'exit' the program exits
+    except:
+        print("Error, Not a valid code. Please use the first 4 digits on the left of each line. 1520 = an example.")
+        ask_var = input("Press enter to restart or type the word 'exit' to exit the program\n")
+        if ask_var == 'exit':
+            break
